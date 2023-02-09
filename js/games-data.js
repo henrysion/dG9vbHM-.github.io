8750c505c4931421645f416ce9f633476f4da6c1
henrysion <114241448+henrysion@users.noreply.github.com>
2023-02-09 22:03:31 +0800

    ss

--- js/games-data.js
+++ js/games-data.js
@@ -1,54 +1,54 @@
 var gamesData = [ {
-	"url": "https://toolbox.csdshumeipai.xyz/unlock-music/",
-	"imgSrc": "https://toolbox.csdshumeipai.xyz/imgs/unlock-music.png",
+	"url": "https://tools.henrysion.xyz/unlock-music/",
+	"imgSrc": "https://tools.henrysion.xyz/imgs/unlock-music.png",
 	"name": "音频解锁",
 	"tags": ["Music"],
 	"id": "d1eeb899810e4c359805ad860e405847"
 },{
-	"url": "https://toolbox.csdshumeipai.xyz/shou/",
-	"imgSrc": "https://toolbox.csdshumeipai.xyz/imgs/shou.png",
+	"url": "https://tools.henrysion.xyz/shou/",
+	"imgSrc": "https://tools.henrysion.xyz/imgs/shou.png",
 	"name": "兽音译者",
 	"tags": ["文字处理"],
 	"id": "d1eeb899810e4c359805ad860e405847"
 },{
-	"url": "https://toolbox.csdshumeipai.xyz/base64/",
-	"imgSrc": "https://toolbox.csdshumeipai.xyz/imgs/base64.png",
+	"url": "https://tools.henrysion.xyz/base64/",
+	"imgSrc": "https://tools.henrysion.xyz/imgs/base64.png",
 	"name": "Base64编解码",
 	"tags": ["文字处理"],
 	"id": "d1eeb899810e4c359805ad860e405847"
 },{
-	"url": "https://toolbox.csdshumeipai.xyz/momo/",
-	"imgSrc": "https://toolbox.csdshumeipai.xyz/imgs/momo.png",
+	"url": "https://tools.henrysion.xyz/momo/",
+	"imgSrc": "https://tools.henrysion.xyz/imgs/momo.png",
 	"name": "摸摸_动图生成",
 	"tags": ["图片处理"],
 	"id": "d1eeb899810e4c359805ad860e405847"
 },{
-	"url": "https://toolbox.csdshumeipai.xyz/video/",
-	"imgSrc": "https://toolbox.csdshumeipai.xyz/imgs/video.png",
+	"url": "https://tools.henrysion.xyz/video/",
+	"imgSrc": "https://tools.henrysion.xyz/imgs/video.png",
 	"name": "影视解析",
 	"tags": ["影音娱乐"],
 	"id": "d1eeb899810e4c359805ad860e405847"
 },{
-	"url": "https://toolbox.csdshumeipai.xyz/radio/",
-	"imgSrc": "https://toolbox.csdshumeipai.xyz/imgs/radio.png",
+	"url": "https://tools.henrysion.xyz/radio/",
+	"imgSrc": "https://tools.henrysion.xyz/imgs/radio.png",
 	"name": "在线广播",
 	"tags": ["影音娱乐"],
 	"id": "d1eeb899810e4c359805ad860e405847"
 },{
-	"url": "https://toolbox.csdshumeipai.xyz/morse_code/",
-	"imgSrc": "https://toolbox.csdshumeipai.xyz/imgs/morse_code.png",
+	"url": "https://tools.henrysion.xyz/morse_code/",
+	"imgSrc": "https://tools.henrysion.xyz/imgs/morse_code.png",
 	"name": "摩尔斯电码",
 	"tags": ["通讯"],
 	"id": "d1eeb899810e4c359805ad860e405847"
 },{
-	"url": "https://toolbox.csdshumeipai.xyz/proxy/",
-	"imgSrc": "https://toolbox.csdshumeipai.xyz/imgs/proxy.png",
+	"url": "https://tools.henrysion.xyz/proxy/",
+	"imgSrc": "https://tools.henrysion.xyz/imgs/proxy.png",
 	"name": "网页代理",
 	"tags": ["网络工具"],
 	"id": "d1eeb899810e4c359805ad860e405847"
 },{
-	"url": "https://toolbox.csdshumeipai.xyz/chatgpt/",
-	"imgSrc": "https://toolbox.csdshumeipai.xyz/imgs/chatgpt.png",
+	"url": "https://tools.henrysion.xyz/chatgpt/",
+	"imgSrc": "https://tools.henrysion.xyz/imgs/chatgpt.png",
 	"name": "ChatGPT",
 	"tags": ["人工智能"],
 	"id": "d1eeb899810e4c359805ad860e405847"
